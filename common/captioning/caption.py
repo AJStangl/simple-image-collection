fb020774042f78f8ce56f50ba8f5236cb34ed059
AJStangl <ajstangl@gmail.com>
2023-05-07 08:17:00 -0400

    Updating Things

--- common/captioning/caption.py
+++ common/captioning/caption.py
@@ -5,11 +5,10 @@ import requests
 
 
 class BlipCaption:
-	def __init__(self, device_number: str = "0"):
+	def __init__(self, device_name: str = "cuda"):
 		self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-		self.device = torch.device(f"cuda:{device_number}")
-		self.model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large",
-																  torch_dtype=torch.float16).to(self.device)
+		self.device = torch.device(device_name)
+		self.model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", torch_dtype=torch.float16).to(self.device)
 
 	def caption_image(self, image_path: str) -> str:
 		try:
