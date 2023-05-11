4bdd8d22c5c8628b6805994dce535c0cafe43bbe
AJStangl <ajstangl@gmail.com>
2023-05-11 16:55:04 -0400

    Adding the fancy

--- common/captioning/azure_descriptions.py
+++ common/captioning/azure_descriptions.py
@@ -1,15 +1,18 @@
-import logging
+# import logging
 import os
-from ctypes import Union
+import random
+from typing import Union
 
+import pandas
 from adlfs import AzureBlobFileSystem
 from azure.ai.vision import VisionServiceOptions, VisionSource, ImageAnalysisOptions, ImageAnalysisFeature, \
 	ImageAnalyzer, ImageAnalysisResultReason, ImageAnalysisResultDetails, ImageAnalysisErrorDetails
 from azure.cognitiveservices.vision.computervision import ComputerVisionClientConfiguration, ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import ImageDescription
-
-logging.basicConfig(level=logging.DEBUG)
-logging.getLogger(__name__)
+from azure.ai.vision import ImageAnalysisResult
+#
+# logging.basicConfig(level=logging.DEBUG)
+# logging.getLogger(__name__)
 
 
 class AzureCaption(object):
@@ -20,7 +23,7 @@ class AzureCaption(object):
 		self.__vision_client: ComputerVisionClient = ComputerVisionClient(endpoint=self.__endpoint, credentials=self.__credentials)
 		self.__file_system_reference: AzureBlobFileSystem = file_system_reference
 
-	def generate_ai_tags(self, image_path: str) -> Union([ImageDescription, None]):
+	def generate_ai_tags(self, image_path: str) -> Union[ImageDescription, None]:
 		try:
 			describe_response: ImageDescription = self.__vision_client.describe_image(
 				self.__file_system_reference.url(image_path), max_candidates=10, raw=True)
@@ -29,13 +32,10 @@ class AzureCaption(object):
 			logging.error(e)
 			return None
 
-	def image_analysis_sample_analyze(self, image_path: str) -> Union(ImageAnalysisResultDetails, None):
-		"""
-		Analyze image from file, all features, synchronous (blocking)
-		"""
+	def _get_image_analyzer(self, image_path: str = None, image_url: str = None) -> ImageAnalyzer:
 
 		service_options: VisionServiceOptions = VisionServiceOptions(self.__endpoint, self.__subscription_key)
-		vision_source: VisionSource = VisionSource(filename=image_path)
+		vision_source: VisionSource = VisionSource(filename=image_path, url=image_url)
 		analysis_options: ImageAnalysisOptions = ImageAnalysisOptions()
 
 		analysis_options.features = (
@@ -55,71 +55,17 @@ class AzureCaption(object):
 		analysis_options.gender_neutral_caption = False
 
 		image_analyzer: ImageAnalyzer = ImageAnalyzer(service_options, vision_source, analysis_options)
+		return image_analyzer
 
-		print()
-		print(" Please wait for image analysis results...")
-		print()
+	def image_analysis(self, image_path: str) -> Union[ImageAnalysisResultDetails, None]:
+		"""
+		Analyze image from file, all features, synchronous (blocking)
+		"""
 
-		result = image_analyzer.analyze()
+		analyzer = self._get_image_analyzer(image_path=None, image_url=image_path)
 
-		# Checks result.
-		if result.reason == ImageAnalysisResultReason.ANALYZED:
-			print(" Image height: {}".format(result.image_height))
-			print(" Image width: {}".format(result.image_width))
-			print(" Model version: {}".format(result.model_version))
+		result_analysis: ImageAnalysisResult = analyzer.analyze()
 
-			if result.caption is not None:
-				print(" Caption:")
-				print("   '{}', Confidence {:.4f}".format(result.caption.content, result.caption.confidence))
+		result_details: ImageAnalysisResultDetails = ImageAnalysisResultDetails.from_result(result_analysis)
 
-			if result.dense_captions is not None:
-				print(" Dense Captions:")
-				for caption in result.dense_captions:
-					print("   '{}', {}, Confidence: {:.4f}".format(caption.content, caption.bounding_box,
-																   caption.confidence))
-
-			if result.objects is not None:
-				print(" Objects:")
-				for object in result.objects:
-					print("   '{}', {}, Confidence: {:.4f}".format(object.name, object.bounding_box,
-																   object.confidence))
-
-			if result.tags is not None:
-				print(" Tags:")
-				for tag in result.tags:
-					print("   '{}', Confidence {:.4f}".format(tag.name, tag.confidence))
-
-			if result.people is not None:
-				print(" People:")
-				for person in result.people:
-					print("   {}, Confidence {:.4f}".format(person.bounding_box, person.confidence))
-
-			if result.crop_suggestions is not None:
-				print(" Crop Suggestions:")
-				for crop_suggestion in result.crop_suggestions:
-					print("   Aspect ratio {}: Crop suggestion {}"
-						  .format(crop_suggestion.aspect_ratio, crop_suggestion.bounding_box))
-
-			if result.text is not None:
-				print(" Text:")
-				for line in result.text.lines:
-					points_string = "{" + ", ".join([str(int(point)) for point in line.bounding_polygon]) + "}"
-					print("   Line: '{}', Bounding polygon {}".format(line.content, points_string))
-					for word in line.words:
-						points_string = "{" + ", ".join([str(int(point)) for point in word.bounding_polygon]) + "}"
-						print("     Word: '{}', Bounding polygon {}, Confidence {:.4f}"
-							  .format(word.content, points_string, word.confidence))
-
-			result_details: ImageAnalysisResultDetails = ImageAnalysisResultDetails.from_result(result)
-			return result_details
-
-		else:
-
-			error_details = ImageAnalysisErrorDetails.from_result(result)
-			print(" Analysis failed.")
-			print("   Error reason: {}".format(error_details.reason))
-			print("   Error code: {}".format(error_details.error_code))
-			print("   Error message: {}".format(error_details.message))
-			print(" Did you set the computer vision endpoint and key?")
-			return None
-
+		return result_details
