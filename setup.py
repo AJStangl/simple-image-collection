5ec0f123e1f741b5357d421740bb05c933fbd371
AJStangl <ajstangl@gmail.com>
2023-05-15 16:52:15 -0400

    f

--- setup.py
+++ setup.py
@@ -3,33 +3,33 @@ import os
 
 
 def read(fname):
-	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
-	name="simple-collection",
-	version="0.0.1",
-	author="AJ Stangl",
-	author_email="ajstangl@gmail.com",
-	description="Code for my damn self",
-	license="MIT",
-	keywords="GPT2",
-	include_package_data=True,
-	url="https://example.com",
-	packages=['common',
-			  'common/captioning',
-			  'common/progress',
-			  'common/schemas',
-			  'common/storage'],
+    name="simple-collection",
+    version="0.0.1",
+    author="AJ Stangl",
+    author_email="ajstangl@gmail.com",
+    description="Code for my damn self",
+    license="MIT",
+    keywords="GPT2",
+    include_package_data=True,
+    url="https://example.com",
+    packages=['common',
+              'common/captioning',
+              'common/progress',
+              'common/schemas',
+              'common/storage'],
 
-	long_description=read('README.md'),
-	classifiers=[
-		"Topic :: Utilities",
-		"License :: MIT License",
-	],
-	entry_points={
-		'console_scripts': [
-			'simple-collection = main.py',
-		],
-	},
+    long_description=read('README.md'),
+    classifiers=[
+        "Topic :: Utilities",
+        "License :: MIT License",
+    ],
+    entry_points={
+        'console_scripts': [
+            'simple-collection = main.py',
+        ],
+    },
 )
